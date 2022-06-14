49153db55521b2ad66c2aceacb5bd591b513d5fb
Ilya <ganychev92@gmail.com>
2022-06-14 12:09:43 +0300

    added Doors and doors logic

--- Assets/Scripts/InteractableObjects/Door.cs
+++ Assets/Scripts/InteractableObjects/Door.cs
@@ -10,7 +10,7 @@ using AosSdk.ThirdParty.QuickOutline.Scripts;
 
 public class Door : BaseObject
 {
-    [SerializeField] private GameObject _handle;
+    [SerializeField] private bool _inside;
     private bool _canAction = true;
     private bool _open = false;
     public override void OnClicked(InteractHand interactHand)
@@ -23,42 +23,52 @@ public class Door : BaseObject
         Player.Instance.CanMove = false;
         GetComponent<Collider>().isTrigger = true;
         _canAction = false;
-        if (!value)
-        {
-            Debug.Log("Открываем");
-            int z = 0;
-            while (z <= 90)
+            if (!_inside)
             {
-                _handle.transform.localRotation = Quaternion.Euler(0, 0, z);
-                yield return new WaitForSeconds(0.01f);
-                z++;
+                if (!value)
+                {
+                    int y = 0;
+                    while (y >= -90)
+                    {
+                    transform.localEulerAngles += new Vector3(0, 1, 0);
+                        yield return new WaitForSeconds(0.01f);
+                        y--;
+                    }
+                }
+                else
+                {
+                    int y = -90;
+                    while (y <= 0)
+                    {
+                    transform.localEulerAngles -= new Vector3(0, 1, 0);
+                    yield return new WaitForSeconds(0.01f);
+                        y++;
+                    }
+                }
             }
-            int y = 0;
-            while (y >= -90)
+            else
             {
-                transform.localRotation = Quaternion.Euler(0, y, 0);
-                yield return new WaitForSeconds(0.01f);
-                y--;
+                if (!value)
+                {
+                    int y = 0;
+                    while (y <= 90)
+                    {
+                    transform.localEulerAngles -= new Vector3(0, 1, 0);
+                    yield return new WaitForSeconds(0.01f);
+                        y++;
+                    }
+                }
+                else
+                {
+                    int y = 90;
+                    while (y >= 0)
+                    {
+                    transform.localEulerAngles += new Vector3(0, 1, 0);
+                    yield return new WaitForSeconds(0.01f);
+                        y--;
+                    }
+                }
             }
-        }
-        else
-        {
-            Debug.Log("Закрываем");
-            int y = -90;
-            while (y <= 0)
-            {
-                transform.localRotation = Quaternion.Euler(0, y, 0);
-                yield return new WaitForSeconds(0.01f);
-                y++;
-            }
-            int z = 90;
-            while (z >= 0)
-            {
-                _handle.transform.localRotation = Quaternion.Euler(0, 0, z);
-                yield return new WaitForSeconds(0.01f);
-                z--;
-            }
-        }
         Player.Instance.CanMove = true;
         _canAction = true;
         GetComponent<Collider>().isTrigger = false;
