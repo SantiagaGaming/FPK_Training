1d43ee18fc6b7cd7c686576b66fef75f9cd7c6a9
Ilya <ganychev92@gmail.com>
2022-06-20 11:37:30 +0300

    Handles added
    added handles logic to rotate doors

--- Assets/Scripts/InteractableObjects/Doors/RotateDoor.cs
+++ Assets/Scripts/InteractableObjects/Doors/RotateDoor.cs
@@ -11,13 +11,18 @@ using AosSdk.ThirdParty.QuickOutline.Scripts;
 public class RotateDoor : Door
 {
     [SerializeField] private bool _inside;
+    [SerializeField] private bool _x;
+    [SerializeField] private bool _down;
 
     override protected IEnumerator UseDoor(bool value)
     {
         DoorAction(true);
+
             if (!_inside)
             {
-                if (!value)
+            if (handle != null)
+                StartCoroutine(RotateHandle());
+            if (!value)
                 {
                     int y = 0;
                     while (y >= -90)
@@ -29,7 +34,7 @@ public class RotateDoor : Door
                 }
                 else
                 {
-                    int y = -90;
+                int y = -90;
                     while (y <= 0)
                     {
                     transform.localEulerAngles -= new Vector3(0, 1, 0);
@@ -40,7 +45,9 @@ public class RotateDoor : Door
             }
             else
             {
-                if (!value)
+            if (handle != null)
+                StartCoroutine(RotateHandle());
+            if (!value)
                 {
                     int y = 0;
                     while (y <= 90)
@@ -52,7 +59,7 @@ public class RotateDoor : Door
                 }
                 else
                 {
-                    int y = 90;
+                int y = 90;
                     while (y >= 0)
                     {
                     transform.localEulerAngles += new Vector3(0, 1, 0);
@@ -65,5 +72,35 @@ public class RotateDoor : Door
         if (open)
             open = false;
         else open = true;
+    }
+    private IEnumerator RotateHandle()
+    {
+            int rot = 0;
+            while (rot <= 45)
+            {
+            if(!_x && !_down)
+                handle.transform.localEulerAngles += new Vector3(0, 0, 1);
+            else if(!_x && _down)
+                handle.transform.localEulerAngles -= new Vector3(0, 0, 1);
+            else if(_x && !_down)
+                handle.transform.localEulerAngles -= new Vector3(1, 0, 0);
+            else
+                handle.transform.localEulerAngles += new Vector3(1, 0, 0);
+            yield return new WaitForSeconds(0.008f);
+                rot++;
+            }
+            while (rot>=0)
+        {
+            if(!_x && !_down)
+             handle.transform.localEulerAngles -= new Vector3(0, 0, 1);
+            else if (!_x && _down)
+                handle.transform.localEulerAngles += new Vector3(0, 0, 1);
+            else if (_x && !_down)
+                handle.transform.localEulerAngles += new Vector3(1, 0, 0);
+            else
+                handle.transform.localEulerAngles -= new Vector3(1, 0, 0);
+            yield return new WaitForSeconds(0.008f);
+            rot--;
+        }
     }
 }
