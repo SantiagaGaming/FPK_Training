c68488a0d59ae91d436fb2a7d33ded270bdbf0e5
Alexander <sherstoboev93@gmail.com>
2023-05-24 10:31:56 +0300

    Optimization project

---
+++ Assets/Scripts/Other/LightSwitchObject.cs
@@ -0,0 +1,30 @@
+using AosSdk.Core.PlayerModule.Pointer;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitchObject : BaseObject
+{
+    [SerializeField] private GameObject _light;
+    [SerializeField] private Animator _animator;
+    private bool _on = false;
+    public override void OnClicked(InteractHand interactHand)
+    {
+        base.OnClicked(interactHand);
+        if(!_on)
+        {
+            _animator.SetTrigger("OnAnim");
+            _light.SetActive(true);
+            _on = true;
+
+        }
+        else
+        {
+            _animator.SetTrigger("OffAnim");
+            _light.SetActive(false);
+            _on = false;
+        }
+       
+
+    }
+}
