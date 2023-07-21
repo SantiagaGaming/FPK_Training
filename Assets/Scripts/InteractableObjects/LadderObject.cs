0359073c16fa01428e4acc84178b00969b30b885
Alexander <sherstoboev93@gmail.com>
2023-07-21 11:07:22 +0300

    Ladder object added

--- Assets/Scripts/InteractableObjects/LadderObject.cs
+++ Assets/Scripts/InteractableObjects/LadderObject.cs
@@ -5,26 +5,52 @@ using UnityEngine;
 
 public class LadderObject : BaseObject
 {
-    private Animator _animator;
-    [Space]
+    [SerializeField] private LockEnabableObject _lockObject;
+    [SerializeField] private Collider _polkaCollider;
+    private Animator _animator;   
     private bool _open = false;
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        StartCoroutine(BrokenStateLadder());
     }
 
     public override void OnClicked(InteractHand interactHand)
     {
-        if (!_open)
+        if (_lockObject.CurrentState == "Idle" && !_open)
         {
             _animator.SetTrigger("Open");
             _open= true;
+            _polkaCollider.enabled= false;
         }
-        else
+        else if (_lockObject.CurrentState == "Idle" && _open)
         {
             _animator.SetTrigger("Close");
             _open = false;
+            _polkaCollider.enabled = true;
+        }
+        else if(_lockObject.CurrentState == "Broken" && !_open)
+        {
+            _animator.SetTrigger("Broken");
+            _open = true;
+            _polkaCollider.enabled = false;
+            
+        }
+        else
+        {
+            _animator.SetTrigger("BrokenClose");
+            _open = false;
+            _polkaCollider.enabled = true;
+           
         }
 
+    }
+    private IEnumerator BrokenStateLadder()
+    {
+        yield return new WaitForSeconds(2);
+        if (_lockObject.CurrentState == "Broken")
+        {
+            _animator.SetTrigger("BrokenClose");
+        }
     }
 }
