0e427796ffa1af319a35d2d624a332507675b4d9
Alexander <sherstoboev93@gmail.com>
2023-08-16 15:40:26 +0300

    Added new menu

--- Assets/Scripts/Models/CheckListItem.cs
+++ Assets/Scripts/Models/CheckListItem.cs
@@ -10,33 +10,33 @@ public class CheckListItem : MonoBehaviour
 {
     [SerializeField] private Sprite _checkInSprite;
     [SerializeField] private Sprite _checkOutSprite;
-    [SerializeField] private Image _currentSprite;
-    [SerializeField] private Image _img;
+
+    private Image _img;
     private Button _button;
 
     [SerializeField] private BreakObject _obj;
-    public string Id  => _obj.ObjectId;
+    public string Id => _obj.ObjectId;
     public bool Checked { get; private set; }
-    
+
     private void Awake()
     {
         _button = GetComponent<Button>();
+        _img = GetComponent<Image>();
         _button.onClick.AddListener(Check);
+
     }
 
     private void Check()
     {
-        if(!Checked)
+        if (!Checked)
         {
-            _currentSprite.sprite = _checkInSprite;
+            _img.sprite = _checkInSprite;
             Checked = true;
-            _img.color = Color.blue;
         }
         else
         {
-            _currentSprite.sprite = _checkOutSprite;
+            _img.sprite = _checkOutSprite;
             Checked = false;
-            _img.color = Color.white;
         }
     }
 }
