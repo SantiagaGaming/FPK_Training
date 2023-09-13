86ab60d3e6e7607a75285601d49e26a36d69ea25
Alexander <sherstoboev93@gmail.com>
2023-09-13 12:50:34 +0300

    Edits

--- Assets/Scripts/InteractableObjects/BrokenObject/LocalRotationObject.cs
+++ Assets/Scripts/InteractableObjects/BrokenObject/LocalRotationObject.cs
@@ -9,10 +9,11 @@ public class LocalRotationObject : SearchableObject
     [SerializeField] private float _y;
     [SerializeField] private float _z;
     [SerializeField] private Door _door;
+    [SerializeField] private Collider _collider;
     public override void EnableObject(bool value)
     {
         base.EnableObject(value);
-
+        if(_collider!= null) { _collider.enabled = value; }
         if (!value)
             Obj.transform.localRotation = Quaternion.Euler(_x, _y, _z);
         if (_door == null)
