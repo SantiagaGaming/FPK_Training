1226663c882f618c228cd7926f5a749841f1916b
Alexander <sherstoboev93@gmail.com>
2023-09-18 10:40:17 +0300

    Date panal add

---
+++ Assets/Scripts/Other/DateTimeNow.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DateTimeNow : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _dateText;
+    [SerializeField] private TextMeshProUGUI _timeText;
+
+    private DateTime _currentDate = DateTime.Now;
+    private TimeSpan Time;
+
+    private void Start()
+    {
+        _dateText.text = _currentDate.ToString("dd/MM/yyyy");
+        _timeText.text = _currentDate.Hour.ToString("D2")+ " : "+ _currentDate.Minute.ToString("D2");
+        
+    }
+}
