a4998ced42809164179780cb1d068dcc5bf1f9a9
Alexander <sherstoboev93@gmail.com>
2023-10-03 17:03:51 +0300

    preloader added

--- Assets/Scripts/View/SubmitCheckListButton.cs
+++ Assets/Scripts/View/SubmitCheckListButton.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-[RequireComponent(typeof (Button))]
+[RequireComponent(typeof(Button))]
 public class SubmitCheckListButton : MonoBehaviour
 {
     [SerializeField] private EditButton _editButton;
-   
+
+    private float _delay = 0.2f;
     private Button _button;
     public UnityAction SubmitButtonClick;
     private void Awake()
@@ -16,19 +17,24 @@ public class SubmitCheckListButton : MonoBehaviour
     }
     private void Start()
     {
-        _button.onClick.AddListener(() => { SubmitButtonClick?.Invoke(); }); 
+        _button.onClick.AddListener(() => { SubmitButtonClick?.Invoke(); });
     }
 
     public void HideButtons()
     {
+        StartCoroutine(Delay());
+        
+    }
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(_delay);
         gameObject.SetActive(false);
         _editButton.gameObject.SetActive(true);
         foreach (var button in _editButton.Buttons)
         {
             button.enabled = false;
-            button.image.color = new Color(1, 1, 1,0.47f);
+            button.image.color = new Color(1, 1, 1, 0.47f);
 
         }
     }
-
 }
